31e5157b4b5befe667b48982d9d297f24c672c50
Hüseyin Tokpınar <hhtokpinar@yahoo.com>
2019-11-11 13:57:17 +0300

    Delete model.g.form.dart

--- lib/model/model.g.form.dart
+++
@@ -1,9 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-// **************************************************************************
-// SqfEntityFormGenerator
-// **************************************************************************
-
-/*
-FORM WIDGETS OF YOUR MODEL WILL COME HERE SOON
-*/
