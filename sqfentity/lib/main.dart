1f6e494e4e70417b4bb92a7f4694393aa132bf24
manimaran <mani@tinisoft.in>
2024-05-09 17:55:37 +0530

    Chore : create db on app support directory

--- sqfentity/lib/main.dart
+++ sqfentity/lib/main.dart
@@ -59,11 +59,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
