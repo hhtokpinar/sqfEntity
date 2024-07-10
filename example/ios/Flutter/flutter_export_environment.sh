06af8f30ccb0a2207c084b5df6405dc0cdc64066
hhtokpinar <hhtokpinar@yahoo.com>
2024-07-10 15:36:39 +0300

    pub upgrade

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Volumes/DEPO/SDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/DEPO/Projeler/sqfEntity3/example"
+export "FLUTTER_ROOT=C:\Flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Flutter\sqfEntity\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.3.0"
 export "FLUTTER_BUILD_NUMBER=1.3.0"
